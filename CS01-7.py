9a647516c911f190ab7d5a2fc548f40417ba4343
Fookfick <Nattakit1919@gmail.com>
2021-07-23 09:16:37 +0700

    Update CS01-7.py

--- CS01-7.py
+++ CS01-7.py
@@ -1,9 +1,9 @@
 A=int(input("enter ur grade"))
 if A<=10:
-    print("failed")
+    print("ไม่ผ่าน")
 elif A<=20:
-    print("Improve")
+    print("ปรับปรุง")
 elif A<=30:
-    print("Passed")
+    print("ผ่าน")
 else:
-    print("error")
+    print("error") 
